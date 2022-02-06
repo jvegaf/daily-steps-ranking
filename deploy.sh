3f287e37faf20cee6d42c6f4597a6a6f780f6be1
Jose Vega <josevega234@gmail.com>
2022-02-06 21:53:02 +0100

    hotfix
    Signed-off-by: Jose Vega <josevega234@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,5 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jvegaf/daily-steps-ranking.git main:gh-pages
+git push -f git@github.com:jvegaf/daily-steps-ranking.git master:gh-pages
 cd -
